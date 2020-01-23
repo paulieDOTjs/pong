7c02d003dcd4ed60cf23cf29e8fdf0bd526cf738
Paul Hendrickson <PaulRobHendrickson@gmail.com>
2020-01-23 14:25:28 -0600

    paddles working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import React from "react";
-// import "./styles.css";
+import React, { useState, useEffect } from "react";
 import Paddle from "./components/Paddle/Paddle";
 import Ball from "./components/Ball/Ball";
 import "./App.css";
 
 export default function App() {
+  const [paddleOneY, setPaddleOneY] = useState(0);
+  const [paddleTwoY, setPaddleTwoY] = useState(0);
+
+  function keepInBox(value) {
+    if (value < 0) {
+      value = 0;
+    }
+    if (value > 85) {
+      value = 85;
+    }
+    return value;
+  }
+
+  function handleKey(e) {
+    const char = e.key.toLowerCase();
+    if (char === "w" || char === "s") {
+      setPaddleOneY(keepInBox(paddleOneY + (char === "w" ? -5 : 5)));
+    }
+    if (char === "p" || char === "l") {
+      setPaddleTwoY(keepInBox(paddleTwoY + (char === "p" ? -5 : 5)));
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("keypress", handleKey);
+    return () => window.removeEventListener("keypress", handleKey);
+  }, [paddleTwoY, paddleOneY]);
+
   return (
     <div className="App">
       <Ball />
-      <Paddle />
-      <Paddle isPlayerTwo />
+      <Paddle paddleY={paddleOneY} />
+      <Paddle isPlayerTwo paddleY={paddleTwoY} />
     </div>
   );
 }
--- src/components/paddle/paddle.js
+++ src/components/paddle/paddle.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./Paddle.css";
 
-function Paddle({ isPlayerTwo }) {
-  return <div className={isPlayerTwo ? "paddle player2" : "paddle player1"} />;
+function Paddle({ isPlayerTwo, paddleY }) {
+
+  return (
+    <div
+      className={isPlayerTwo ? "paddle player2" : "paddle player1"}
+      style={{ top: `${paddleY}%` }}
+    />
+  );
 }
 
 export default Paddle;
